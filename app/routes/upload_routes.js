7da87fc0ac0aee06821a60ad528cc123d045a9fd
mauramaybe11 <mauramay11@gmail.com>
2022-05-02 13:12:03 -0400

    Set up Upload routes
    -edited the ACL and ContentType
    -tested upload in postman
    -in AWS Bucket edited ACL permissions
    Co-authored-by: Ruo, CJ, Cody

--- app/routes/upload_routes.js
+++ app/routes/upload_routes.js
@@ -1,12 +1,27 @@
+// we installed an npm package called multer with command npm install multer this allows us to store files on the body, One option for sending files with the body is buffer, this will allow us to send files as buffer
 const express = require('express')
+// we are importing the npm package multer that we installed
+const multer = require('multer')
+const storage = multer.memoryStorage()
+// they use the upload variable and there are different methods there is upload.fields, upload.array, upload.single accepts a single image and name
+const upload = multer({ storage: storage })
 const Upload = require('../models/upload')
 const router = express.Router()
 
-router.post('/uploads', (req, res, next) => {
+const s3Upload = require('../../lib/s3_upload')
+
+// Create route for an upload
+// multer will add body and files object
+// Are we going to have to require token in this route or that won't work because the way we connect to AWS?
+router.post('/uploads', upload.single('upload'), (req, res, next) => {
+  console.log(req.body, req.file)
+  s3Upload(req.file)
+    .then(awsFile => {
+      return Upload.create({ url: awsFile.Location })
+    })
   // this is basically sending an upload object with key of URL and value of a url for example google
-  Upload.create(req.body.upload)
-    .then(upload => {
-      res.status(201).json({upload})
+    .then(uploadDoc => {
+      res.status(201).json({upload: uploadDoc})
     })
 })
 // once route is created need to require in server.js
