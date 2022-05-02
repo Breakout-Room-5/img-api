a2e4cdea74bdebef24e30defff39a4b26dbd275d
ruzheng <ruzheng1990@gmail.com>
2022-05-02 13:20:21 -0400

    Code Along with Eron

--- app/routes/upload_routes.js
+++ app/routes/upload_routes.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const Upload = require('../models/upload')
 const router = express.Router()
+const multer = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+const s3Upload = require('../../lib/s3_upload')
 
-router.post('/uploads', (req, res, next) => {
-  // req.body => { upload: {url: 'www.google.com' } }
-  Upload.create(req.body.upload)
-    .then(upload => {
+router.post('/uploads', upload.single('upload'), (req, res, next) => {
+  s3Upload(req.file)
+    .then(awsFile => {
+      return Upload.create({url: awsFile.Location})
+    })
+    .then((upload) => {
       res.status(201).json({ upload })
     })
     .catch(next)
 })
+
+module.exports = router
