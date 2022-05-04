0315e25b9b40f6251c5d372b05dfe83c3d55c6cb
ruzheng <ruzheng1990@gmail.com>
2022-05-04 09:37:22 -0400

    Reactivate upload routes

--- app/routes/upload_routes.js
+++ app/routes/upload_routes.js
@@ -1,20 +1,20 @@
-// const express = require('express')
-// const Upload = require('../models/upload')
-// const router = express.Router()
-// const multer = require('multer')
-// const storage = multer.memoryStorage()
-// const upload = multer({ storage: storage })
-// const s3Upload = require('../../lib/s3_upload')
+const express = require('express')
+const Upload = require('../models/upload')
+const router = express.Router()
+const multer = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+const s3Upload = require('../../lib/s3_upload')
 
-// router.post('/uploads', upload.single('upload'), (req, res, next) => {
-//   s3Upload(req.file)
-//     .then(awsFile => {
-//       return Upload.create({url: awsFile.Location})
-//     })
-//     .then((upload) => {
-//       res.status(201).json({ upload })
-//     })
-//     .catch(next)
-// })
+router.post('/uploads', upload.single('upload'), (req, res, next) => {
+  s3Upload(req.file)
+    .then(awsFile => {
+      return Upload.create({url: awsFile.Location})
+    })
+    .then((upload) => {
+      res.status(201).json({ upload })
+    })
+    .catch(next)
+})
 
-// module.exports = router
+module.exports = router
